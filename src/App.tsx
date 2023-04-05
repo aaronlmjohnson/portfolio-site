89a8f3d304af057e33f92074d8e687f829b0abf3
Aaron Johnson <aaronlmjohnson307@gmail.com>
2023-04-05 11:21:31 -0500

    added about-me section

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import About from './components/About';
 
 function App() {
   return (
     <div className="container">
       <Navbar links={["Home", "About", "Projects", "Contact"]}/>
       <Hero />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.tsx
@@ -0,0 +1,16 @@
+const About = ()=>{
+    return (
+        <div>
+            <img src="#" id="about-image"></img>
+            <div id="about-me-text">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quam ab maxime minus 
+                    placeat eveniet architecto voluptatum nisi magnam laudantium. 
+                    Laboriosam voluptate quos sapiente earum, quia consequatur. Sit, accusantium ipsam!
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About;
