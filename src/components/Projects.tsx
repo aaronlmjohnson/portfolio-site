a91855e7d95722a8c025d48dd3cae5772c6a49a7
Aaron Johnson <aaronlmjohnson307@gmail.com>
2023-04-15 08:14:21 -0500

    added patch file for projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,18 +1,27 @@
 import uniqid from 'uniqid';
 
-const ProjectDiv = ()=>{
+interface ProjectsProps {
+    src: string;
+    description: string;
+}
+
+const ProjectDiv = ({src, description}: ProjectsProps)=>{
     return (
         <div className="project" key={uniqid()}>
-            <img className="project-preview"></img>
-            <p className="project-description"></p>
+            <img className="project-preview" src="#"></img>
+            <p className="project-description">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                 Dolor, reiciendis veniam beatae blanditiis optio necessitatibus aut eveniet omnis ipsa, 
+                 quis fuga nihil dolore adipisci deleniti iure architecto dolorem sequi labore.
+            </p>
         </div>
     )
 }
 
-const Projects = ()=>{
+const Projects = (projectsProps: ProjectsProps[])=>{
     return (
         <div id="projects">
-            
+
         </div>
     )
 }
