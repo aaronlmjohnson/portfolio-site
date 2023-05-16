e4ed0cd773e1f545fc18bc86751cad4ea88f8acc
Aaron <aaronlmjohnson307@gmail.com>
2023-05-16 10:26:17 -0400

    added placeholder project

--- src/assets/data/ProjectData.tsx
+++ src/assets/data/ProjectData.tsx
@@ -2,7 +2,11 @@ const ProjectData =
 [
     {
         "src": require('../images/video_game_finder_app.png'),
-        "description": "a site for information on current and older video games"
+        "description": "A site for information on current and older video games."
+    },
+    {
+        "src": require('../images/project_image.png'),
+        "description": "Another website in the making."
     }
 ]
 
