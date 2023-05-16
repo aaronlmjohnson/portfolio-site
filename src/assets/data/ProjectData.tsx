c361992ff07c37ea3f0d6f87d055921faf301c7b
Aaron <aaronlmjohnson307@gmail.com>
2023-05-16 10:33:24 -0400

    added name and links to ProjectData

--- src/assets/data/ProjectData.tsx
+++ src/assets/data/ProjectData.tsx
@@ -1,12 +1,22 @@
 const ProjectData = 
 [
     {
+        "name": "Find a Game App",
         "src": require('../images/video_game_finder_app.png'),
-        "description": "A site for information on current and older video games."
+        "description": "A site for information on current and older video games.",
+        "links": {
+            main:"https://aaronlmjohnson.github.io/video-game-recommendation-app/index.html",
+            github:"https://github.com/aaronlmjohnson/video-game-recommendation-app"
+        }
     },
     {
+        "name": "Placeholder",
         "src": require('../images/project_image.png'),
-        "description": "Another website in the making."
+        "description": "A site in the making.",
+        "links": {
+            main:"https://react.dev/",
+            github:"https://github.com/aaronlmjohnson/"
+        }
     }
 ]
 
